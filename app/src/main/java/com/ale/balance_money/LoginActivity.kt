fc498b796e0424ab6e8d468b6acd0682918a9a0d
Alejandro <alegonzalez21195@gmail.com>
2020-06-19 14:55:43 -0600

    Add button to login with facebook

--- app/src/main/java/com/ale/balance_money/LoginActivity.kt
+++ app/src/main/java/com/ale/balance_money/LoginActivity.kt
@@ -1,12 +1,38 @@
 package com.ale.balance_money
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.facebook.login.widget.LoginButton
+import java.util.*
+
 
 class LoginActivity : AppCompatActivity() {
-
+    var email:String = "email";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val loginButton = findViewById<LoginButton>(R.id.login_button)
+        loginButton.setReadPermissions(Arrays.asList(email));
+        var callbackManager = CallbackManager.Factory.create()
+
+        LoginManager.getInstance().registerCallback(callbackManager,
+            object : FacebookCallback<LoginResult?> {
+                override fun onSuccess(loginResult: LoginResult?) {
+                    // App code
+                }
+
+                override fun onCancel() {
+                    // App code
+                }
+
+                override fun onError(exception: FacebookException) {
+                    // App code
+                }
+            })
     }
 }
