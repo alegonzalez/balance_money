e26480ec3dec4fff0fd696fefe4649a737cc267d
aalvarado <alegonzalez21195@gmail.com>
2020-06-26 11:08:19 -0500

    Unit test class person

---
+++ balance_money/app/src/test/java/com/ale/balance_money/PersonTest.kt
@@ -0,0 +1,52 @@
+package com.ale.balance_money
+
+
+import junit.framework.Assert.assertTrue
+import junit.framework.TestCase.assertFalse
+import org.junit.Test
+
+class PersonTest{
+
+    private val person:Person = Person("Alejandro Alvarado","","")
+    /*
+    test without email should return false
+    */
+    @Test
+    fun emailEmptyValidator() {
+            assertFalse("empty email", person.writeNewUser(person))
+    }
+    /*
+        test without name should return false
+        */
+    @Test
+    fun nameValidator() {
+        person.email = "aalvarado@tabacon.com"
+        person.name = "";
+        assertFalse("empty name", person.writeNewUser(person))
+    }
+    /*
+        test email is valid
+        */
+    @Test
+    fun emailValid() {
+        person.email = "aalvarado@tabacon.com"
+        assertTrue("email is valid", person.validateEmail(person.email))
+    }
+    /*
+            test email is invalid
+            */
+    @Test
+    fun emailInvalid() {
+        person.email = "aalvaradotabacon.com"
+        assertFalse("Email is invalid", person.validateEmail(person.email))
+    }
+    /*
+     test name and email with data, should return true
+     */
+    @Test
+    fun nameAndEmailValidator() {
+        person.email = "aalvarado@tabacon.com"
+        person.name = "Alejandro Alvarado Gonzalez";
+        assertTrue("Validacion con datos erroneos", person.writeNewUser(person))
+    }
+}
