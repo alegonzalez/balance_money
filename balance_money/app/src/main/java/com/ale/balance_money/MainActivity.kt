55f48d7361337b761a334b60927f6259c2c9bd2e
aalvarado <alegonzalez21195@gmail.com>
2020-06-26 11:09:29 -0500

    layout menu and main

---
+++ balance_money/app/src/main/java/com/ale/balance_money/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.ale.balance_money
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.blogspot.atifsoftwares.animatoolib.Animatoo
+
+
+class MainActivity : AppCompatActivity() {
+    private val person = Person()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        val handler:Handler =  Handler()
+        handler.postDelayed({
+            checkLogin()
+        }, 5000)
+
+    }
+
+    /**
+     * This function if user is login or not
+     */
+    fun checkLogin(){
+        if(person.isLoggedIn()){
+            /*
+            val intentMenu = Intent(this, MenuActivity::class.java)
+            startActivity(intentMenu)
+            Animatoo.animateZoom(this);
+
+             */
+            val intentLogin = Intent(this, LoginActivity::class.java)
+            startActivity(intentLogin)
+            Animatoo.animateZoom(this);
+        }else{
+            val intentLogin = Intent(this, LoginActivity::class.java)
+            startActivity(intentLogin)
+            Animatoo.animateZoom(this);
+        }
+    }
+
+    override fun onDestroy() {
+        finish()
+        super.onDestroy()
+    }
+}
